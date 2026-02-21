12cec1421fe9d02c54a0928cd77b8a77d98de97a
Bui Tan Phat <phatbtts02863@gmail.com>
2026-02-21 23:31:38 +0700

    update slider

--- javascript/home/slider/slider.js
+++ javascript/home/slider/slider.js
@@ -1,11 +1,13 @@
 const slider = document.querySelector(".slider");
 import { fetchapi } from "../setapi.js";
-fetchapi("https://web-gophim-project-default-rtdb.firebaseio.com/slider_section1.json")
-.then((result)=>{
+fetchapi(
+  "https://web-gophim-project-default-rtdb.firebaseio.com/slider_section1.json",
+).then((result) => {
   let html = "";
-  result.forEach(item =>{
-    if(item.slider == "active"){
-      html += `<div class="slide active">
+  let htmlmini = "";
+  const slidermini = document.querySelector(".slider__mini");
+  result.forEach((item) => {
+    html += `<div class="slide ${item.slider == "active" ? "active" : ""}">
           <div class="item">
             <div class="inner__img">
               <img
@@ -14,7 +16,7 @@ fetchapi("https://web-gophim-project-default-rtdb.firebaseio.com/slider_section1
             </div>
             <div class="inner__content">
               <h3>
-                <a href="#">${item.title}</a>
+                <a href="#${item.title}">${item.title}</a>
               </h3>
               <a class="meta__tag" href="#45">${item.meta_tag}</a>
               <div class="inner__meta">
@@ -31,55 +33,30 @@ fetchapi("https://web-gophim-project-default-rtdb.firebaseio.com/slider_section1
                 ${item.description}
                 </p>
               </div>
-              <a class="btn_playvideo" href="#">
+              <a class="btn_playvideo" href="#${item.title}">
                 <i class="fa-solid fa-play"></i>
               </a>
             </div>
           </div>
         </div>`;
-        slider.innerHTML = html;
-    }
-    if(item.slider !== "active"){
-      html += `<div class="slide">
-          <div class="item">
-            <div class="inner__img">
-              <img
-                src="${item.img}"
-              />
-            </div>
-            <div class="inner__content">
-              <h3>
-                <a href="#">${item.title}</a>
-              </h3>
-              <a class="meta__tag" href="#45">${item.meta_tag}</a>
-              <div class="inner__meta">
-                <p class="meta__item">GOPHIM</p>
-                <p>${item.year}</p>
-                <p>${item.session}</p>
-                <p>${item.practice}</p>
-              </div>
-              <div class="meta__fullhd">
-                <span>Full HD</span>
-              </div>
-              <div class="description">
-                <p>
-                ${item.description}
-                </p>
-              </div>
-              <a class="btn_playvideo" href="#">
-                <i class="fa-solid fa-play"></i>
-              </a>
-            </div>
+    slider.innerHTML = html;
+    htmlmini += `
+          <div class="item ${item.slider == "active" ? "active" : ""}">
+          <img src="${item.img}"/>
           </div>
-        </div>`;
-        slider.innerHTML = html;
-    }
+          `;
+    slidermini.innerHTML = htmlmini;
   });
-  let index = 0;
-  const slides = document.querySelectorAll(".slide");
-  setInterval(() => {
-  slides[index].classList.remove("active");
-  index = (index + 1) % slides.length;
-  slides[index].classList.add("active");
-}, 10000);
+  // slider mini
+const slide = document.querySelectorAll(".slide");
+const item = document.querySelectorAll(".slider__mini .item");
+item.forEach((e, index) => {
+  e.addEventListener("click", (e) => {
+    slide.forEach((del) => del.classList.remove("active"));
+    item.forEach((del) => del.classList.remove("active"));
+    slide[index].classList.add("active");
+    e.currentTarget.classList.add("active");
+  });
+});
+// end slider mini
 });
