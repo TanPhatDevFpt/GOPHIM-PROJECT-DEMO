57b2d00e8cd91892a110d10696209355881155ed
Bui Tan Phat <phatbtts02863@gmail.com>
2026-02-21 18:06:30 +0700

    fix animation slider

--- javascript/home/slider/slider.js
+++ javascript/home/slider/slider.js
@@ -77,7 +77,7 @@ fetchapi("https://web-gophim-project-default-rtdb.firebaseio.com/slider_section1
   });
   let index = 0;
   const slides = document.querySelectorAll(".slide");
-setInterval(() => {
+  setInterval(() => {
   slides[index].classList.remove("active");
   index = (index + 1) % slides.length;
   slides[index].classList.add("active");
