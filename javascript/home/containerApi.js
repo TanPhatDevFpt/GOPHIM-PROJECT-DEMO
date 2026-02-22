e21aa0daf66b67f708c9178e216ab24c8e2957bb
Bui Tan Phat <phatbtts02863@gmail.com>
2026-02-22 17:22:10 +0700

    fix container API

--- javascript/home/containerApi.js
+++ javascript/home/containerApi.js
@@ -1,4 +1,4 @@
 const api_base = "https://web-gophim-project-default-rtdb.firebaseio.com/";
 export const API ={
-    slider: `${api_base}/slider.json`,
+    slider: `${api_base}/slider_section1.json`,
 }
