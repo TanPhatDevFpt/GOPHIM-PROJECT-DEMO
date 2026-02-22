e08916705fc72d56b8fb898f2bc5d3c88064cb59
Bui Tan Phat <phatbtts02863@gmail.com>
2026-02-23 00:23:19 +0700

    update category

---
+++ javascript/home/category/category.js
@@ -0,0 +1,21 @@
+import { fetchapi } from "../setapi.js";
+import { API } from "../containerApi.js";
+const category = document.querySelector(".inner__catergory");
+const api = `${API.category}`;
+fetchapi(api)
+.then((result)=>{
+    let htmlcategory = "";
+    result.forEach(item=>{
+        htmlcategory += `
+        <div class="category" style="background: ${item.gradient}">
+            <a href="#${item.title}" class="category__item">
+              <p class="title">${item.title}</p>
+              <p class="description">
+                Xem chủ đề <i class="fa-solid fa-angle-right"></i>
+              </p>
+            </a>
+          </div>
+        `;
+    });
+    category.innerHTML = htmlcategory;
+});
