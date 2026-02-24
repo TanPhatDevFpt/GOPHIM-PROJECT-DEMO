d030f046c89f7178276ad37e0949291654aaa608
Bui Tan Phat <phatbtts02863@gmail.com>
2026-02-24 17:16:46 +0700

    update swiper korea

---
+++ javascript/home/apiswiper/swiperkorea.js
@@ -0,0 +1,30 @@
+import { fetchapi } from "../setapi.js";
+import { API } from "../containerApi.js";
+const swiperWrapper = document.querySelector(".swiper-wrapper");
+const api = `${API.swiperkorea}`;
+fetchapi( api)
+  .then((data) => {
+    let html = "";
+    data.data.forEach((item)=>{
+      html += `
+            <div class="swiper-slide">
+              <a href="#${item.title}">
+                <div class="inner__img">
+                  <img
+                    src="${item.img}"
+                  />
+                  <div class="description">
+                    <span>PĐ.</span>
+                    <strong>${item.pd}</strong>
+                  </div>
+                </div>
+                <div class="inner__content">
+                  <a href="#">${item.title}</a>
+                  <p>${item.tag}</p>
+                </div>
+              </a>
+            </div>
+      `;
+    });
+    swiperWrapper.innerHTML = html;
+});
--- javascript/home/containerApi.js
+++ javascript/home/containerApi.js
@@ -2,4 +2,5 @@ const api_base = "https://web-gophim-project-default-rtdb.firebaseio.com/";
 export const API ={
     slider: `${api_base}/slider_section1.json`,
     category: `${api_base}/category.json`,
+    swiperkorea: `${api_base}/swiper_korea.json`
 }
